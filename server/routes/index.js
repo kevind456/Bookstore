21f8834ae192bc49aa2664fe7bab10b8fe380c86
liamprudenn <liam.pruden@ontariotechu.net>
2024-11-11 15:28:35 -0500

    Added book tracker list display into landing page.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Book = require('../model/book'); // (changed to fix 404 error when passing)
 
-/* GET index page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
+/* GET Home page with All Books */
+router.get('/', async (req, res, next) => {
+  try {
 
-/* GET Home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    const BookList = await Book.find();
+    res.render('index', { 
+      title: 'Home', 
+      BookList: BookList 
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { 
+      title: 'Home', 
+      BookList: [] // passes empty array in case of error
+    });
+  }
 });
 
 module.exports = router;
